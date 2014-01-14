2d88a1b0657cd1f9d0ac1cd017fb40ce4c4657ab
Peter Grman <peter.grman@gmail.com>
2014-01-14 20:09:33 +0100

    Solution for problem 7
    Also again refactored the prime creating so that it can be used in
    problem 7

--- 0001-0050/primes.py
+++ 0001-0050/primes.py
@@ -4,12 +4,11 @@ not part of project-euler just a helper function to create prime numbers which a
 
 import math
 
-
-def create_primes(highest_prime):
+def create_all_primes(condition_to_continue):
     primes = [2, 3]
     i = 3
 
-    while i < highest_prime:
+    while condition_to_continue(primes, i):
         i += 2
         si = math.sqrt(i)
         is_prime = True
@@ -24,3 +23,11 @@ def create_primes(highest_prime):
             primes += [i]
 
     return primes
+
+
+def create_primes(highest_prime):
+    return create_all_primes(lambda primes, i: i < highest_prime)
+
+
+def create_n_primes(amount):
+    return create_all_primes(lambda primes, i: len(primes) < amount)
