3e10a4344b90a94e8e92d716162544a6126d43e5
Peter Grman <peter.grman@gmail.com>
2014-01-24 19:23:17 +0100

    Solution for problem 23

---
+++ 0001-0050/23-Non-abundant_sums.py
@@ -0,0 +1,38 @@
+"""
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example,
+the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n
+and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16,
+the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis,
+it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers.
+However, this upper limit cannot be reduced any further by analysis even though it is known that
+the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+"""
+
+import divisors
+
+allPossibleIntegers = range(0, 28124)
+abundantNumbers = []
+
+
+def is_abundant_number(number):
+    return sum(divisors.get_proper_divisors(number)) > number
+
+
+def check_abundant_sums(number):
+    for abundant_number in abundantNumbers:
+        if abundant_number + number < len(allPossibleIntegers):
+            allPossibleIntegers[abundant_number + number] = 0
+
+
+for i in xrange(1, 28124):
+    if is_abundant_number(i):
+        abundantNumbers += [i]
+        check_abundant_sums(i)
+
+print str(sum(allPossibleIntegers))
