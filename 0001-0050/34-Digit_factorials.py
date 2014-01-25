d1ab93f0a46cfc198d86c57d8657baf87eb1ae61
Peter Grman <peter.grman@gmail.com>
2014-01-26 00:01:37 +0100

    Solution for problem 34

---
+++ 0001-0050/34-Digit_factorials.py
@@ -0,0 +1,31 @@
+"""
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+
+
+def split_into_digits(number):
+    while number > 0:
+        yield number % 10
+        number /= 10
+
+
+fact = {}
+
+for x in xrange(0, 10):
+    f = 1
+    for y in xrange(2, x + 1):
+        f *= y
+
+    fact[x] = f
+
+factSum = 0
+
+for i in xrange(10, 1000*100):  # found out experimentally
+    if i == sum([fact[j] for j in split_into_digits(i)]):
+        factSum += i
+
+print str(factSum)
